d7039f7baeb2ebca988cfc39945ea728b147857c
Ramonprog <programador.ramon@gmail.com>
2024-05-16 19:04:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { CalendarComponent } from "./components/Calendar";
+
+function App() {
+  return (
+    <>
+      <CalendarComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Calendar/index.jsx
@@ -0,0 +1,45 @@
+import moment from "moment";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import { Container } from "./styles";
+import { useState } from "react";
+
+moment.locale("pt-br");
+const DragAndDropCalendar = withDragAndDrop(Calendar);
+const localizer = momentLocalizer(moment);
+
+export function CalendarComponent() {
+  const [events, setEvents] = useState([
+    {
+      id: 0,
+      title: "Paulinha Abelha",
+      start: new Date(2024, 4, 14, 10, 0),
+      end: new Date(2024, 4, 14, 12, 0),
+      desc: "Encerramento de obra",
+      type: "singer",
+    },
+    {
+      id: 1,
+      title: "Marlus Viana",
+      start: new Date(2024, 4, 15),
+      end: new Date(2024, 4, 15),
+      desc: "Encerramento de obra",
+      type: "singer",
+    },
+  ]);
+
+  return (
+    <Container>
+      <DragAndDropCalendar
+        localizer={localizer}
+        defaultDate={moment().toDate()}
+        defaultView="month"
+        events={events}
+        style={{ height: "600px" }}
+        resizable={false}
+      />
+    </Container>
+  );
+}
